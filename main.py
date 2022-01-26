16e3a1cd24a6b4e5040c63b3153ca2064e797751
Duyanhdda <anh.haduyanh@hcmut.edu.vn>
2022-01-26 14:56:42 +0700

    Update 2 cach lay vi tri

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+
+
 print("Xin chào ThingsBoard")
 import paho.mqtt.client as mqttclient
 import time
 import json
-
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+driver = webdriver.Chrome()
+driver.get("https://www.where-am-i.co/")
 BROKER_ADDRESS = "demo.thingsboard.io"
 PORT = 1883
 THINGS_BOARD_ACCESS_TOKEN = "tpmf3c9NcxvIH3rq9FGk"
@@ -46,11 +51,41 @@ temp = 30
 humi = 50
 light_intesity = 100
 counter = 0
-latitude = 10
-longtitude = 106
+from requests import get
+import json
+import urllib.request
+
+ip_address = get('https://api.ipify.org').content.decode('utf8')
+
+
+
+# Cách 1
+# GEO_IP_API_URL  = 'http://ip-api.com/json/'
+#
+# # Can be also site URL like this : 'google.com'
+# IP_TO_SEARCH    = ip_address
+#
+# # Creating request object to GeoLocation API
+# req             = urllib.request.Request(GEO_IP_API_URL+IP_TO_SEARCH)
+# # Getting in response JSON
+# response        = urllib.request.urlopen(req).read()
+# # Loading JSON from text to object
+# json_response   = json.loads(response.decode('utf-8'))
+# latitude = json_response['lat']
+# longtitude = json_response['lon']
+
+
+# cách 2
+def locate():
+    element = driver.find_element(By.XPATH, '/html/body/main/div/div[2]/div[2]/div/table/tbody/tr[3]/td/span')
+    data = str(element.text)
+    latlon = data.split(', ')
+    return latlon
+longtitude = locate()[1]
+latitude= locate()[0]
 while True:
-    collect_data = {'temperature': temp, 'humidity': humi, 'light':light_intesity
-                    ,'longtitude': longtitude,'latitude':latitude}
+    collect_data = {'temperature': temp, 'humidity': humi, 'light': light_intesity
+                    , 'longitude': longtitude, 'latitude': latitude}
     temp += 1
     humi += 1
     light_intesity += 1
